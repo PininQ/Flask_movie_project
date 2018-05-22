84724958a1945e1c8ae022f6ae8968a35748f6d1
qinbin52qiul <2538323140@qq.com>
2018-05-22 18:44:26 +0800

    会员登录页面的搭建

--- app/home/views.py
+++ app/home/views.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 __author__ = 'QB'
 from . import home
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 
 # 调用蓝图(app/home/views.py)
 @home.route("/")
 def index():
     return render_template("home/index.html")
+
+
+# 登录
+@home.route('/login/')
+def login():
+    return render_template('home/login.html')
+
+
+# 退出
+@home.route('/logout/')
+def logout():
+    # 重定向到home模块下的登录
+    return redirect(url_for('home.login'))
