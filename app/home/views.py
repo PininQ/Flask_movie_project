313a0a2947f0cea861c37854040032bfeceaa81e
qinbin52qiul <2538323140@qq.com>
2018-05-23 00:11:47 +0800

    会员中心界面搭建，包括会员中心、修改密码、评论记录、登录日志、收藏电影

--- app/home/views.py
+++ app/home/views.py
@@ -27,3 +27,33 @@ def logout():
 @home.route('/register/')
 def register():
     return render_template('home/register.html')
+
+
+# 会员中心
+@home.route('/user/')
+def user():
+    return render_template('home/user.html')
+
+
+# 修改密码
+@home.route('/pwd/')
+def pwd():
+    return render_template('home/pwd.html')
+
+
+# 评论记录
+@home.route('/comments/')
+def comments():
+    return render_template('home/comments.html')
+
+
+# 登录日志
+@home.route('/loginlog/')
+def loginlog():
+    return render_template('home/loginlog.html')
+
+
+# 收藏电影
+@home.route('/moviecol/')
+def moviecol():
+    return render_template('home/moviecol.html')
