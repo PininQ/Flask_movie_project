41fbb60fa20da93e68867f55122ed2eb02ee90e1
qinbin52qiul <2538323140@qq.com>
2018-05-23 18:52:37 +0800

    管理员登录页面搭建,包括后台登录和退出登录

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 __author__ = 'QB'
 from . import admin
+from flask import render_template, redirect, url_for
 
 
 # 调用蓝图(app/admin/views.py)
 @admin.route("/")
 def index():
     return "<h1 style='color:red'>this is admin</h1>"
+
+
+# 后台登录
+@admin.route("/login/")
+def login():
+    return render_template('admin/login.html')
+
+
+# 退出登录
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for('admin.login'))
