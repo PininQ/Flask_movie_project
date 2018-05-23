2748c36a1bd3d22b7c6a3688b2911e8983d0b40d
qinbin52qiul <2538323140@qq.com>
2018-05-23 21:20:38 +0800

    标签管理页面(添加标签和标签列表)搭建完毕

--- app/admin/views.py
+++ app/admin/views.py
@@ -27,3 +27,15 @@ def logout():
 @admin.route("/pwd/")
 def pwd():
     return render_template('admin/pwd.html')
+
+
+# 编辑标签
+@admin.route("/tag/add/")
+def tag_add():
+    return render_template('admin/tag_add.html')
+
+
+# 标签列表
+@admin.route("/tag/list/")
+def tag_list():
+    return render_template('admin/tag_list.html')
