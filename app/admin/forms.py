1b819092a05a2d9625298acea50c1cb1a5374756
qinbin52qiul <2538323140@qq.com>
2018-05-25 21:33:55 +0800

    后台逻辑之电影管理(电影添加)

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -2,9 +2,9 @@
 __author__ = 'QB'
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FileField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, ValidationError
-from app.models import Admin
+from app.models import Admin, Tag
 
 
 class LoginForm(FlaskForm):
@@ -12,24 +12,24 @@ class LoginForm(FlaskForm):
     account = StringField(
         label="账号",
         validators=[
-            DataRequired("账号不能为空")
+            DataRequired("账号不能为空！")
         ],
         description="账号",
         render_kw={
             "class": "form-control",
-            "placeholder": "请输入账号！",
+            "placeholder": "请输入账号",
             # "required": "required"
         }
     )
     pwd = PasswordField(
         label="密码",
         validators=[
-            DataRequired("密码不能为空")
+            DataRequired("密码不能为空！")
         ],
         description="密码",
         render_kw={
             "class": "form-control",
-            "placeholder": "请输入密码！",
+            "placeholder": "请输入密码",
             # "required": "required"
         }
     )
@@ -52,13 +52,119 @@ class TagForm(FlaskForm):
     name = StringField(
         label="标签名称",
         validators=[
-            DataRequired("标签名称不能为空")
+            DataRequired("标签名称不能为空！")
         ],
         description="标签",
         render_kw={
             "class": "form-control",
             "id": "input_name",
-            "placeholder": "请输入标签名称！"
+            "placeholder": "请输入标签名称"
+        }
+    )
+    submit = SubmitField(
+        "添加",
+        render_kw={
+            "class": "btn btn-primary"
+        }
+    )
+
+
+class MovieForm(FlaskForm):
+    """电影管理"""
+    title = StringField(
+        label="片名",
+        validators=[
+            DataRequired("片名不能为空！")
+        ],
+        description="片名",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入片名"
+        }
+    )
+    url = FileField(
+        label="文件",
+        validators=[
+            DataRequired("请上传文件！")
+        ],
+        description="文件"
+    )
+    info = TextAreaField(
+        label="简介",
+        validators=[
+            DataRequired("简介不能为空！")
+        ],
+        description="简介",
+        render_kw={
+            "class": "form-control",
+            "rows": 10
+        }
+    )
+    logo = FileField(
+        label="封面",
+        validators=[
+            DataRequired("请上传封面！")
+        ],
+        description="封面"
+    )
+    star = SelectField(
+        label="星级",
+        validators=[
+            DataRequired("请选择星级！")
+        ],
+        # star的数据类型
+        coerce=int,
+        choices=[(1, '1星'), (2, '2星'), (3, '3星'), (4, '4星'), (5, '5星')],
+        description="星级",
+        render_kw={
+            "class": "form-control",
+        }
+    )
+    # 标签选择数据库中已存在的标签
+    tag_id = SelectField(
+        label="标签",
+        validators=[
+            DataRequired("请选择标签！")
+        ],
+        coerce=int,
+        choices=[(v.id, v.name) for v in Tag.query.all()],
+        description="标签",
+        render_kw={
+            "class": "form-control",
+        }
+    )
+    area = StringField(
+        label="上映地区",
+        validators=[
+            DataRequired("上映地区不能为空！")
+        ],
+        description="上映地区",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入上映地区"
+        }
+    )
+    length = StringField(
+        label="片长",
+        validators=[
+            DataRequired("片长不能为空！")
+        ],
+        description="片长",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入片长"
+        }
+    )
+    release_time = StringField(
+        label="上映时间",
+        validators=[
+            DataRequired("上映时间不能为空！")
+        ],
+        description="上映时间",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请选择上映时间",
+            "id": "input_release_time"
         }
     )
     submit = SubmitField(
--- app/admin/views.py
+++ app/admin/views.py
@@ -2,10 +2,14 @@
 __author__ = 'QB'
 from . import admin
 from flask import render_template, redirect, url_for, flash, session, request
-from app.admin.forms import LoginForm, TagForm
-from app.models import Admin, Tag
+from app.admin.forms import LoginForm, TagForm, MovieForm
+from app.models import Admin, Tag, Movie
 from functools import wraps
-from app import db
+from app import db, app
+from werkzeug.utils import secure_filename
+import os
+import uuid
+from datetime import datetime
 
 
 def admin_login_req(f):
@@ -18,6 +22,13 @@ def admin_login_req(f):
         return f(*args, **kwargs)
 
     return decorated_function
+
+
+def change_filename(filename):
+    """修改文件名称"""
+    fileinfo = os.path.splitext(filename)
+    filename = datetime.now().strftime("%Y%m%d%H%M%S") + str(uuid.uuid4().hex) + fileinfo[-1]
+    return filename
 
 
 # 调用蓝图(app/admin/views.py)
@@ -126,11 +137,42 @@ def tag_del(id=None):
     return redirect(url_for('admin.tag_list', page=1))
 
 
-@admin.route("/movie/add/")
+@admin.route("/movie/add/", methods=['GET', 'POST'])
 @admin_login_req
 def movie_add():
-    """编辑电影"""
-    return render_template('admin/movie_add.html')
+    """添加电影"""
+    form = MovieForm()
+    if form.validate_on_submit():
+        data = form.data
+        file_url = secure_filename(form.url.data.filename)
+        file_logo = secure_filename(form.logo.data.filename)
+        if not os.path.exists(app.config["UP_DIR"]):
+            # 创建一个多级目录
+            os.makedirs(app.config["UP_DIR"])
+            os.chmod(app.config["UP_DIR"], "rw")
+        url = change_filename(file_url)
+        logo = change_filename(file_logo)
+        # 保存url和logo
+        form.url.data.save(app.config["UP_DIR"] + url)
+        form.logo.data.save(app.config["UP_DIR"] + logo)
+        movie = Movie(
+            title=data['title'],
+            url=url,
+            info=data['info'],
+            logo=logo,
+            star=int(data['star']),
+            playnum=0,
+            commentnum=0,
+            tag_id=(data['tag_id']),
+            area=data['area'],
+            release_time=data['release_time'],
+            length=data['length'],
+        )
+        db.session.add(movie)
+        db.session.commit()
+        flash("添加电影成功！", "ok")
+        return redirect(url_for('admin.movie_add'))
+    return render_template('admin/movie_add.html', form=form)
 
 
 @admin.route("/movie/list/")
