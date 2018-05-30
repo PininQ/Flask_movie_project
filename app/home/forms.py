c5ce0ebd1e4c5ce8b9a1ab31e2a1bb8febb988bf
qinbin52qiul <2538323140@qq.com>
2018-05-30 10:31:43 +0800

    前台逻辑之会员模块(会员注册、会员登录、会员退出)，简单修改前台代码

--- app/home/forms.py
+++ app/home/forms.py
@@ -1,2 +1,124 @@
 # -*- coding: utf-8 -*-
 __author__ = 'QB'
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Regexp, EqualTo, ValidationError
+from app.models import User
+
+
+# 注册
+class RegisterForm(FlaskForm):
+    name = StringField(
+        label="会员名",
+        validators=[
+            DataRequired("会员名不能为空!"),
+        ],
+        description="会员名",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "会员名"
+        }
+    )
+    email = StringField(
+        label="邮箱",
+        validators=[
+            DataRequired("邮箱不能为空!"),
+            Email("邮箱格式不正确！")
+        ],
+        description="邮箱",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "邮箱"
+        }
+    )
+    phone = StringField(
+        label="手机号码",
+        validators=[
+            DataRequired("手机号码不能为空!"),
+            Regexp("(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\\d{8}", message="手机号码格式不正确！")
+        ],
+        description="手机号码",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "手机号码"
+        }
+    )
+    pwd = PasswordField(
+        label="密码",
+        validators=[
+            DataRequired("密码不能为空！")
+        ],
+        description="密码",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "密码",
+        }
+    )
+    repwd = PasswordField(
+        label="确认密码",
+        validators=[
+            DataRequired("确认密码不能为空！"),
+            EqualTo('pwd', message='两次输入密码不一致')
+        ],
+        description="确认密码",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "确认密码",
+        }
+    )
+    submit = SubmitField(
+        "注册",
+        render_kw={
+            "class": "btn btn-lg btn-success btn-block"
+        }
+    )
+
+    def validate_name(self, field):
+        name = field.data
+        user = User.query.filter_by(name=name).count()
+        if user == 1:
+            raise ValidationError("昵称已经存在！")
+
+    def validate_email(self, field):
+        email = field.data
+        user = User.query.filter_by(email=email).count()
+        if user == 1:
+            raise ValidationError("邮箱已经存在！")
+
+    def validate_phone(self, field):
+        phone = field.data
+        user = User.query.filter_by(phone=phone).count()
+        if user == 1:
+            raise ValidationError("手机号码已经存在！")
+
+
+class LoginForm(FlaskForm):
+    name = StringField(
+        label="账号",
+        validators=[
+            DataRequired("账号不能为空!"),
+        ],
+        description="账号",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "会员名/邮箱/手机号码"
+        }
+    )
+    pwd = PasswordField(
+        label="密码",
+        validators=[
+            DataRequired("密码不能为空！")
+        ],
+        description="密码",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "密码",
+        }
+    )
+    submit = SubmitField(
+        "登录",
+        render_kw={
+            "class": "btn btn-lg btn-success btn-block"
+        }
+    )
--- app/home/views.py
+++ app/home/views.py
@@ -1,54 +1,119 @@
 # -*- coding: utf-8 -*-
 __author__ = 'QB'
 from . import home
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, session, request
+from app.home.forms import RegisterForm, LoginForm
+from app.models import User, Userlog
+from werkzeug.security import generate_password_hash
+from app import db, app
+from functools import wraps
+import uuid
 
 
-# 调用蓝图(app/home/views.py)
-@home.route('/login/')
+# 登录装饰器
+def user_login_req(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "user" not in session:
+            # next页面为当前请求的url
+            return redirect(url_for("home.login", next=request.url))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+# 登录：调用蓝图(app/home/views.py)
+@home.route('/login/', methods=['GET', 'POST'])
 def login():
     """登录"""
-    return render_template('home/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        data = form.data
+        users = []
+        users.append(User.query.filter_by(name=data['name']).first())
+        users.append(User.query.filter_by(email=data['name']).first())
+        users.append(User.query.filter_by(phone=data['name']).first())
+        # user = User.query.filter_by(phone=data['name']).first()
+        print(users)
+        # 另一种方法是先去除列表中的None，在进行判断
+        for user in users:
+            if user:
+                if not user.check_pwd(data['pwd']):
+                    flash("密码不正确！", "err")
+                    return redirect(url_for("home.login"))
+                session["user"] = user.name
+                session["user_id"] = user.id
+                userlog = Userlog(
+                    user_id=user.id,
+                    ip=request.remote_addr
+                )
+                db.session.add(userlog)
+                db.session.commit()
+                return redirect(url_for("home.user"))
+        flash("用户不存在，请重新输入!", "err")
+        return redirect(url_for("home.login"))
+    return render_template('home/login.html', form=form)
 
 
 @home.route('/logout/')
 def logout():
     """退出"""
+    session.pop("user", None)
+    session.pop("user_id", None)
     # 重定向到home模块下的登录
     return redirect(url_for('home.login'))
 
 
-@home.route('/register/')
+# 会员注册
+@home.route('/register/', methods=['GET', 'POST'])
 def register():
     """注册"""
-    return render_template('home/register.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        data = form.data
+        user = User(
+            name=data['name'],
+            email=data['email'],
+            phone=data['phone'],
+            pwd=generate_password_hash(data['pwd']),
+            uuid=uuid.uuid4().hex
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("注册成功！", "ok")
+    return render_template('home/register.html', form=form)
 
 
 @home.route('/user/')
+@user_login_req
 def user():
     """会员中心"""
     return render_template('home/user.html')
 
 
 @home.route('/pwd/')
+@user_login_req
 def pwd():
     """修改密码"""
     return render_template('home/pwd.html')
 
 
 @home.route('/comments/')
+@user_login_req
 def comments():
     """评论记录"""
     return render_template('home/comments.html')
 
 
 @home.route('/loginlog/')
+@user_login_req
 def loginlog():
     """登录日志"""
     return render_template('home/loginlog.html')
 
 
 @home.route('/moviecol/')
+@user_login_req
 def moviecol():
     """收藏电影"""
     return render_template('home/moviecol.html')
